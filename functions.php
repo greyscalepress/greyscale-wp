a6b458b2838b5e141730bad1ce43b816955d2a8b
Manuel Schmalstieg <git.code@ms-studio.net>
2013-10-20 17:09:58 +0200

    some admin dashboard improvements

--- functions.php
+++ functions.php
@@ -30,6 +30,54 @@ if ( function_exists( 'register_nav_menu' ) ) {
 // 	register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
 // }
 
+
+/* Dashboard improvement
+******************************/
+
+function tabula_remove_dashboard_widgets()
+{
+	// Globalize the metaboxes array, this holds all the widgets for wp-admin
+	global $wp_meta_boxes;
+	
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+		
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+	// RSS feeds:
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+	
+}
+add_action('wp_dashboard_setup', 'tabula_remove_dashboard_widgets' );
+
+
+/**
+ * Show recent posts : 
+ * https://gist.github.com/ms-studio/6069116
+ */
+
+function wps_recent_posts_dw() {
+?>
+   <ul style="list-style-type: disc;padding-left: 1.5em;">
+     <?php
+          global $post;
+          $args = array( 'numberposts' => 5 );
+          $myposts = get_posts( $args );
+                foreach( $myposts as $post ) :  setup_postdata($post); ?>
+                    <li> <? the_time('j F Y'); ?> – <a href="<?php echo admin_url(); ?>post.php?post=<?php the_ID(); ?>&action=edit"><?php the_title(); ?></a> (<a href="<?php the_permalink(); ?>">visiter</a>)</li>
+          <?php endforeach; ?>
+   </ul>
+<?php
+}
+function add_wps_recent_posts_dw() {
+       wp_add_dashboard_widget( 'wps_recent_posts_dw', __( 'Recent Posts' ), 'wps_recent_posts_dw' );
+}
+add_action('wp_dashboard_setup', 'add_wps_recent_posts_dw' );
+
+
+
+
 function slidesh0w() {
 
 		// Get the post ID
